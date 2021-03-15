1627e703210eea6ec2cbf831a39e3067d7338b05
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-03-15 18:51:06 +0100

    testy_v1 dodanie testowania metody sreach w Binary Serach

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 
 class BinarySearchTest {
 
@@ -12,8 +15,51 @@ class BinarySearchTest {
     void setUp() throws Exception {}
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void test0()
+    {
+        int[] someArray = {1};
+        SearchResult a = BinarySearch.search(1,someArray);
+        assertEquals(true,a.isFound());
+        assertEquals(1,a.getPosition());
     }
-
+    @Test
+    void test1()
+    {
+        int[] someArray = {1};
+        SearchResult a = BinarySearch.search(2,someArray);
+        assertEquals(false,a.isFound());
+        assertEquals(-1,a.getPosition());
+    }
+    @Test
+    void test2()
+    {
+        int[] someArray = {3, 4, 5, 9, 12, 22, 32, 65, 78};
+        SearchResult a = BinarySearch.search(3,someArray);
+        assertEquals(1,a.getPosition());
+        assertEquals(true,a.isFound());
+    }
+    @Test
+    void test3()
+    {
+        int[] someArray = {3, 4, 5, 9, 12, 22, 32, 65, 78};
+        SearchResult a = BinarySearch.search(78,someArray);
+        assertEquals(a.getPosition(),someArray.length);
+        assertEquals(true,a.isFound());
+    }
+    @Test
+    void test4()
+    {
+        int[] someArray = {3, 4, 5, 9, 12, 22, 32, 65, 78};
+        SearchResult a = BinarySearch.search(22,someArray);
+        assertEquals(6,a.getPosition());
+        assertEquals(true,a.isFound());
+    }
+    @Test
+    void test5()
+    {
+        int[] someArray = {3, 4, 5, 9, 12, 22, 32, 65, 78};
+        SearchResult a = BinarySearch.search(-22,someArray);
+        assertEquals(-1,a.getPosition());
+        assertEquals(false,a.isFound());
+    }
 }
